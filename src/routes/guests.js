11053903d487a9d051caf115a3d2a58bf3bfef44
novs123 <novabaldres20@gmail.com>
2025-12-01 16:19:23 +0800

    Refactor guest routes and add booking retrieval

--- src/routes/guests.js
+++ src/routes/guests.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { getGuests, getGuest, createGuest, updateGuest, deleteGuest, getGuestBookings } = require('../controllers/guestController');
+const {
+    getGuests,
+    getGuest,
+    createGuest,
+    updateGuest,
+    deleteGuest
+} = require('../controllers/guestController');
 
-router.route('/').get(getGuests).post(createGuest);
-router.route('/:id').get(getGuest).put(updateGuest).delete(deleteGuest);
-router.route('/:id/bookings').get(getGuestBookings);
+const { getGuestBookings } = require('../controllers/bookingController');
+
+router.route('/')
+    .get(getGuests)
+    .post(createGuest);
+
+router.route('/:id')
+    .get(getGuest)
+    .put(updateGuest)
+    .delete(deleteGuest);
+
+router.route('/:guestId/bookings')
+    .get(getGuestBookings);
 
 module.exports = router;
