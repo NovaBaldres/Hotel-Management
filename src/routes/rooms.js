b0b6984fea66066509697f2d4af0fb15ba45ed27
novs123 <novabaldres20@gmail.com>
2025-12-01 16:19:57 +0800

    Organize room routes with router.route method
    Refactor room routes for better organization and clarity.

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {getRooms, getRoom, createRoom, updateRoom, deleteRoom, getAvailableRooms, getRoomBookings} = require('../controllers/roomController');
+const {
+    getRooms,
+    getRoom,
+    createRoom,
+    updateRoom,
+    deleteRoom
+} = require('../controllers/roomController');
 
-// Available rooms endpoint (must come before /:id)
-router.get('/available', getAvailableRooms);
+router.route('/')
+    .get(getRooms)
+    .post(createRoom);
 
-// Standard CRUD
-router.route('/').get(getRooms).post(createRoom);
-
-// Room bookings history
-router.get('/:id/bookings', getRoomBookings);
-
-// Single room operations
-router.route('/:id').get(getRoom).put(updateRoom).delete(deleteRoom);
+router.route('/:id')
+    .get(getRoom)
+    .put(updateRoom)
+    .delete(deleteRoom);
 
 module.exports = router;
-
 
